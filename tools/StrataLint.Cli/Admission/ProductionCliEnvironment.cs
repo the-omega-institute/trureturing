36d849e06b07f48c4c81195a1a6526f5d30d84b1
Loning <aloning@gmail.com>
2026-09-06 12:16:57 +0800

    Revert "Revert "Merge pull request #5779 from the-omega-institute/integration-digest-l8-20260906""
    This reverts commit 1c7ab8840fa83297a305ef8c76d492651c2ff988.

--- tools/StrataLint.Cli/Admission/ProductionCliEnvironment.cs
+++ tools/StrataLint.Cli/Admission/ProductionCliEnvironment.cs
@@ -489,7 +489,7 @@ internal sealed partial class ProductionCliEnvironment : ICliEnvironment
 
         try
         {
-            return CoverAtomCommand.AlignScribeReceipt(
+            return AlignScribeReceiptCommand.Run(
                 repositoryRoot,
                 repository,
                 leanReportSource,
